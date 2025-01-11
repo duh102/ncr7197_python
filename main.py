6267fea3e67d1fd1e1ea8aea1a3acdd633c39d32
Will Morrow <duh102@gmail.com>
2025-01-11 10:22:00 -0500

    Fix print-and-cut command; add bold, italics, and resizing

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python3
-import ncr7197, argparse
+import ncr7197, argparse, textwrap, sys
+from ncr7197 import AnnotatedText
 
 
 if __name__ != '__main__':
     raise Exception('Must run as executable')
 
-parser = argparse.ArgumentParser()
+def print_stdin(printer):
+    txt = sys.stdin.read()
+    if txt is None:
+        print('Didn\'t read anything')
+    else:
+        for line in txt.splitlines():
+            if len(line.strip()) == 0:
+                printer.print('')
+            else:
+                for subline in textwrap.wrap(line, width=ncr7197.MAX_WIDTH):
+                    printer.print(subline, newlines=0)
+        printer.cut()
+
+def demo(printer):
+    printer.print('Here\'s some text', 2)
+    printer.print(AnnotatedText('Have some text effects!\nSome ') + AnnotatedText('bolded', bold=True) + AnnotatedText(' text, some ') + AnnotatedText('italics', italics=True) + AnnotatedText('\nAnd now various text sizes:\n'))
+    printer.print(AnnotatedText('2X Size', size=ncr7197.SIZE_DOUBLED))
+    printer.print(AnnotatedText('3X Size', size=ncr7197.SIZE_TRIPLED))
+    printer.print(AnnotatedText('4X Size', size=ncr7197.SIZE_QUADRUPLED))
+    printer.cut()
+
+parser = argparse.ArgumentParser(description='Without input, prints a demo receipt showing the capabilities of the library; if given stdin, will print the given text, wrapping to the width of the receipt')
 parser.add_argument('device', help='The device mountpoint of the printer; usually /dev/ttyUSB*')
 
 args = parser.parse_args()
 
 printer = ncr7197.NCR7197(args.device)
-printer.print('Here\'s some text', 2)
-printer.cut()
+
+# stdin has stuff
+if not sys.stdin.isatty():
+    print_stdin(printer)
+else:
+    demo(printer)
--- ncr7197.py
+++ ncr7197.py
@@ -1,11 +1,25 @@
 __CLEAR_PRINTER__ = b'\x10'
 __FULL_KNIFE_CUT__ = b'\x19'
 __PARTIAL_KNIFE_CUT__ = b'\x1A'
+__BOLD_PREFIX__ = b'\x1b\x45\x01'
+__BOLD_POSTFIX__ = b'\x1b\x45\x00'
+__ITALIC_PREFIX__ = b'\x1b\x49\x01'
+__ITALIC_POSTFIX__ = b'\x1b\x49\x00'
+__SIZE_PREFIX__ = b'\x1d\x21'
+__SIZES__ = [b'\x11', b'\x22', b'\x33']
+__SIZE_POSTFIX__ = b'\x1d\x21\x00'
 __MAKE_BEEP__ = b'\x1B\x07'
 __INITIALIZE__ = b'\x1B\x40'
 __SELECT_CUT_MODE_AND_CUT_PREFIX__ = b'\x1D\x56'
+__PRINT_EVERYTHING_AND_PARTIAL_CUT__ = b'\x1d\x56\x41\x06'
 __PRINT_TEST_FORM__ = b'\x1F\x74'
 __PRINT_AND_NEWLINES_PREFIX__ = b'\x1B\x64'
+
+
+# Available character heights
+SIZE_DOUBLED = __SIZES__[0]
+SIZE_TRIPLED = __SIZES__[1]
+SIZE_QUADRUPLED = __SIZES__[2]
 
 # The maximum number of characters on a line in standard mode on 80mm paper
 MAX_WIDTH = 44
@@ -15,6 +29,35 @@ MAX_WIDTH = 44
 # This leaves at least ~ 6 dots of whitespace at the bottom of the receipt, you
 #   may want some more
 PRINT_CUT_OFFSET = 6
+
+class AnnotatedText(object):
+    def __init__(self, text, bold=None, italics=None, size=None):
+        prefix = bytes()
+        postfix = bytes()
+        if bold is None:
+            bold = False
+        if italics is None:
+            italics = False
+        if bold:
+            prefix = __BOLD_PREFIX__
+            postfix = __BOLD_POSTFIX__
+        if italics:
+            prefix += __ITALIC_PREFIX__
+            postfix = __ITALIC_POSTFIX__ + postfix
+        if size is not None:
+            if size not in __SIZES__:
+                raise Exception('Must use one of SIZE_DOUBLED, -TRIPLED, -QUADRUPLED')
+            prefix += __SIZE_PREFIX__ + size
+            postfix = __SIZE_POSTFIX__ + postfix
+
+        self._text = prefix + bytes(text, 'ibm437') + postfix
+
+    def __add__(self, other):
+        self._text += other.get_text()
+        return self
+
+    def get_text(self):
+        return self._text
 
 
 class NCR7197(object):
@@ -28,14 +71,20 @@ class NCR7197(object):
     def print(self, text, newlines=None):
         if newlines is None:
             newlines = 1
-        lines = text.splitlines()
-        with open(self.__device, 'wb') as out:
-            for line in lines:
-                out.write(bytes(line+'\n', 'ascii'))
-            if newlines > 0:
-                out.write(__PRINT_AND_NEWLINES_PREFIX__
-                          + newlines.to_bytes(1,'big'))
+        try:
+            text = text.get_text()
+            with open(self.__device, 'wb') as out:
+                out.write(text + __PRINT_AND_NEWLINES_PREFIX__
+                              + newlines.to_bytes(1,'big') )
+        except AttributeError:
+            lines = text.splitlines()
+            with open(self.__device, 'wb') as out:
+                for line in lines:
+                    out.write(bytes(line+'\n', 'ibm437'))
+                if newlines > 0:
+                    out.write(__PRINT_AND_NEWLINES_PREFIX__
+                              + newlines.to_bytes(1,'big'))
 
     def cut(self):
         with open(self.__device, 'wb') as out:
-            out.write(__PARTIAL_KNIFE_CUT__)
+            out.write(__PRINT_EVERYTHING_AND_PARTIAL_CUT__)
